a21b8445265352e2881444e44f1a5ae7bd1fceb6
sarakhaled95 <architectsarakhaled@outlook.com>
2023-12-22 07:38:42 -0800

    0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
 #creats a file in /tmp
 file { '/tmp/school':
   ensure => 'present',
-  content => "I love Puppet\n",
+  content => "I love Puppet",
   owner => 'www-data',
   group => 'www-data',
   mode => '0744'
