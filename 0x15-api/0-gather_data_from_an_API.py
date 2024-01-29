7f03cff98c0b51a08653eb711ecc600d146ef559
sarakhaled95 <architectsarakhaled@outlook.com>
2024-01-29 11:06:04 -0800

    0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -12,7 +12,6 @@ import json
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
 
-
     employee_id = sys.argv[1]
 
     user_response = requests.get(url + "users/{}".format(employee_id))
@@ -26,7 +25,7 @@ if __name__ == "__main__":
             completed.append(todo.get("title"))
 
     print("Employee {} is done with tasks({}/{}".format(user.get("name"),
-                                                        len(completed), len(todos)))
-
+                                                        len(completed),
+                                                        len(todos)))
     for complete in completed:
         print("\t {}".format(complete))
