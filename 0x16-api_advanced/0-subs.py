31405f4adda39137bda2c94d73b46625ac791340
sarakhaled95 <architectsarakhaled@outlook.com>
2024-02-13 06:48:54 -0800

    0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+function that queries the Reddit API and returns the number of subscribers"""
+import json
+import requests
+from sys import argv
+
+
+def number_of_subscribers(subreddit):
+    """number of subscriber method"""
+    subreddit = argv[1]
+    url = "http://api.reddit.com/r/{}/about".format(subreddit)
+    agent_string = "API-practice-holberton-cardano"
+
+    subs = requests.get(url, headers={'User-Agent': agent_string})
+    if subs.status_code != 200:
+        return (0)
+    else:
+        json_response = subs.json()
+        sub_count = json_response.get('data').get('subscribers')
+        return sub_count
